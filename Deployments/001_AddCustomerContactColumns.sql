7243b1f5dce7e233f696fa5d8663515a0241ea6f
Miladnabi <124622198+Miladnabi@users.noreply.github.com>
2026-08-23 09:41:10 -0700

    Create 001_AddCustomerContactColumns.sql

---
+++ Deployments/001_AddCustomerContactColumns.sql
@@ -0,0 +1,34 @@
+USE CICD_Dev;
+GO
+
+PRINT 'Starting deployment...';
+GO
+
+IF COL_LENGTH('Sales.Customers', 'EmailAddress') IS NULL
+BEGIN
+    ALTER TABLE Sales.Customers
+    ADD EmailAddress VARCHAR(200) NULL;
+
+    PRINT 'EmailAddress added.';
+END
+ELSE
+BEGIN
+    PRINT 'EmailAddress already exists.';
+END;
+GO
+
+IF COL_LENGTH('Sales.Customers', 'PhoneNumber') IS NULL
+BEGIN
+    ALTER TABLE Sales.Customers
+    ADD PhoneNumber VARCHAR(25) NULL;
+
+    PRINT 'PhoneNumber added.';
+END
+ELSE
+BEGIN
+    PRINT 'PhoneNumber already exists.';
+END;
+GO
+
+PRINT 'Deployment complete.';
+GO
